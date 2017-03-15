4ce5304836d664f8f1a61dde98ce115596fca1f4
haythamfawzy <haytham.fawzy@aucegypt.edu>
2017-03-15 12:11:02 +0200

    Most Recent Version before installing Ubuntu

--- shell/Auto_luncher/open_mipshf.sh
+++ shell/Auto_luncher/open_mipshf.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 wmctrl -s 7
-gnome-open /home/hf/mipshf/single/
+gnome-open /home/hf/mipshf/pipeline/
 gnome-open /media/hf/MyWork/AUC/TA/Material/
 google-chrome --profile-directory="Profile 3" --new-window https://bitbucket.org/haythamfawzy/mipshf
 google-chrome --profile-directory="Profile 3" https://www.edaplayground.com/home
 google-chrome --profile-directory="Profile 3" http://www-inst.eecs.berkeley.edu/~cs61c/resources/MIPS_Green_Sheet.pdf
+google-chrome --profile-directory="Profile 3" https://www.gnu.org/software/emacs/refcards/pdf/refcard.pdf
 cd /home/hf/mipshf/single/
 git status
 sudo atom /home/hf/mipshf/
--- shell/Auto_luncher/open_teaching.sh
+++ shell/Auto_luncher/open_teaching.sh
@@ -1,4 +1,5 @@
 wmctrl -s 2
 #open blackboard
 google-chrome --profile-directory="Profile 3" --new-window https://blackboard.aucegypt.edu/webapps/login/
+google-chrome --profile-directory="Profile 3" https://bitbucket.org/
 google-chrome --profile-directory="Profile 3" https://drive.google.com/drive/my-drive &
